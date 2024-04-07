61dd90c69e5541661a1923d851e8c15a3107e38a
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2024-04-07 23:49:40 +0900

    fix(header): remove container

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -9,7 +9,7 @@ import { Suspense } from 'react';
 export default function Header() {
   return (
     <header className="border-b">
-      <div className="container h-16 flex items-center gap-4">
+      <div className="px-4 h-16 flex items-center gap-4">
         <div className="lg:hidden">
           <Suspense>
             <MobileNav />
