8886e772f1ca808c732affcad441bcbf432714de
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2024-04-07 22:05:14 +0900

    feat(layout): add suppress

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" suppressHydrationWarning>
       <body
         className={cn(inter.className, 'h-dvh flex flex-col')}
         suppressHydrationWarning
